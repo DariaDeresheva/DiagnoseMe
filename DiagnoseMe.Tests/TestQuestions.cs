99104bc3926f8388b5b07748540435585db62201
Daria Deresheva <dialia@bk.ru>
2016-04-18 23:51:25 +0700

    Test #1 - Read questions.

---
+++ DiagnoseMe.Tests/TestQuestions.cs
@@ -0,0 +1,14 @@
+﻿using NUnit.Framework;
+
+namespace DiagnoseMe.Tests
+{
+    [TestFixture]
+    internal class TestQuestions
+    {
+        [Test]
+        public void GetAllQuestionStrings()
+        {
+            Assert.DoesNotThrow(() => Doctor.GetAllQuestionStrings(), @"File not found.");
+        }
+    }
+}
---
+++ DiagnoseMe/Doctor.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using System.IO;
+
+namespace DiagnoseMe
+{
+    public class Doctor
+    {
+        public const string FileWithQuestions = @"D:\DiagnoseMe\Docs\Questions.txt";
+
+        public static IEnumerable<string> GetAllQuestionStrings()
+        {
+            return File.ReadLines(FileWithQuestions);
+        } 
+    }
+}
